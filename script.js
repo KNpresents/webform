149f8e73168398f57d9ab7015e471e7b662fe1dd
Kyohei Nagami <105399941+KNpresents@users.noreply.github.com>
2022-12-29 15:52:37 -0800

    Update script.js

--- script.js
+++ script.js
@@ -10,16 +10,21 @@ document.getElementById("form").addEventListener("submit", function(event) {
       const doc = parser.parseFromString(html, "text/html");
       const aElements = doc.querySelectorAll("a");
       for (const a of aElements) {
-        links.push(a.href);
+        links.push({ title: a.textContent, href: a.href });
       }
       console.log(links);
       // リンクを表示するHTMLを作成する処理
       let html = "<ul>";
       for (const link of links) {
-        html += `<li><a href="${link}">${link}</a></li>`;
+        html += `<li><a href="${link.href}">${link.title}</a></li>`;
       }
       html += "</ul>";
       // リンクを表示する
-      document.body.innerHTML = html;
+      document.getElementById("result").innerHTML = html;
     });
+});
+
+// ホームボタンをクリックしたときに、検索画面に戻る
+document.getElementById("home-button").addEventListener("click", function() {
+  window.location.href = "/";
 });

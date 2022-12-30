a79545d1cb263e26a2e45fb311c0c5156d8eeddf
Kyohei Nagami <105399941+KNpresents@users.noreply.github.com>
2022-12-29 16:12:14 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,30 @@
-document.getElementById("form").addEventListener("submit", function(event) {
-  event.preventDefault();
-  const url = document.getElementById("url").value;
-  fetch(url)
-    .then(response => response.text())
-    .then(html => {
-      // HTMLコードからリンクを抽出する処理
-      const links = [];
-      const parser = new DOMParser();
-      const doc = parser.parseFromString(html, "text/html");
-      const aElements = doc.querySelectorAll("a");
-      for (const a of aElements) {
-        links.push({ title: a.textContent, href: a.href });
-      }
-      console.log(links);
-      // リンクを表示するHTMLを作成する処理
-      let html = "<ul>";
-      for (const link of links) {
-        html += `<li><a href="${link.href}">${link.title}</a></li>`;
-      }
-      html += "</ul>";
-      // リンクを表示する
-      document.getElementById("result").innerHTML = html;
-    });
-});
+function searchLinks() {
+  // Get the URL from the form input
+  var url = document.getElementById("url").value;
 
-// ホームボタンをクリックしたときに、検索画面に戻る
-document.getElementById("home-button").addEventListener("click", function() {
-  window.location.href = "/";
-});
+  // Use JavaScript to scan the website and extract the links
+  // ...
+
+  // Display the list of links, grouped by title
+  var resultsDiv = document.getElementById("results");
+  resultsDiv.innerHTML = ""; // Clear the results div
+  for (var title in linksByTitle) {
+    var titleDiv = document.createElement("div");
+    titleDiv.innerHTML = "<h2>" + title + "</h2>";
+    resultsDiv.appendChild(titleDiv);
+
+    var linksList = document.createElement("ul");
+    for (var i = 0; i < linksByTitle[title].length; i++) {
+      var link = linksByTitle[title][i];
+      var linkItem = document.createElement("li");
+      linkItem.innerHTML = "<a href='" + link + "'>" + link + "</a>";
+      linksList.appendChild(linkItem);
+    }
+    resultsDiv.appendChild(linksList);
+  }
+}
+
+function goHome() {
+  // Navigate back to the search page
+  window.location.href = "index.html";
+}
